841ee12405a3f55a510a2e8dcc8cb389a58fbfcf
Ekong Ikpe <webaplications5050@gmail.com>
2026-04-07 07:33:55 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'gnoke-market-v1';
+const ASSETS = [
+  './', '../',
+  './index.html', './settings.html', './about.html',
+  '../style.css', '../global.png', '../manifest.json',
+  '../js/state.js', '../js/theme.js', '../js/ui.js',
+  '../js/db.js', '../js/app.js',
+  './menu.js',
+];
+
+self.addEventListener('install',  e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(() => {})));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
